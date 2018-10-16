3e789b70f927e2d672731cca0d4856ceb6a94311
Olli Tietäväinen <ollit@vaadin.com>
2018-10-16 11:01:49 +0300

    simplified example

--- src/main/java/org/vaadin/olli/ArrayTest.java
+++ src/main/java/org/vaadin/olli/ArrayTest.java
@@ -1,20 +1,11 @@
 package org.vaadin.olli;
 
-import java.util.List;
-
-import com.vaadin.flow.component.ClientCallable;
 import com.vaadin.flow.component.Synchronize;
-import com.vaadin.flow.templatemodel.TemplateModel;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.dependency.HtmlImport;
 import com.vaadin.flow.component.polymertemplate.PolymerTemplate;
+import com.vaadin.flow.templatemodel.TemplateModel;
 
-/**
- * A Designer generated component for the array-test.html template.
- *
- * Designer will add and remove fields with @Id mappings but
- * does not overwrite or otherwise change this file.
- */
 @Tag("array-test")
 @HtmlImport("array-test.html")
 public class ArrayTest extends PolymerTemplate<ArrayTest.ArrayTestModel> {
@@ -23,22 +14,21 @@ public class ArrayTest extends PolymerTemplate<ArrayTest.ArrayTestModel> {
      * Creates a new ArrayTest.
      */
     public ArrayTest() {
-        getElement().synchronizeProperty("itemlist","itemlist-changed");
-        getElement().synchronizeProperty("itemlist","changed");
-        getElement().synchronizeProperty("itemlist","change");
-        getElement().addPropertyChangeListener("itemlist", e->{
+        getElement().synchronizeProperty("itemlist", "itemlist-changed");
+        getElement().synchronizeProperty("itemlist", "changed");
+        getElement().synchronizeProperty("itemlist", "change");
+        getElement().addPropertyChangeListener("itemlist", e -> {
             System.out.println("Itemlist changed: " + getItems());
         });
     }
 
-
-    @Synchronize(value = {"itemlist-changed"}, property="itemlist")
+    @Synchronize(value = { "itemlist-changed" }, property = "itemlist")
     public String getItems() {
         String itemlist = getElement().getProperty("itemlist");
         return itemlist;
     }
 
-       /**
+    /**
      * This model binds properties between ArrayTest and array-test.html
      */
     public interface ArrayTestModel extends TemplateModel {
